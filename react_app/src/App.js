71f0bac5760a254be8326d6773b3783590f8bf25
Serhiy-Plekan <serhiyplx@gmail.com>
2021-04-24 23:50:44 +0300

    xls file support

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -5,31 +5,41 @@ import './App.css';
 function App() {
   const [file, setFile] = useState(null);
   const [serverData, setServerData] = useState({});
+  const [error, setError] = useState('')
   const [correlation, setCorrelation] = useState(0);
 
   useEffect(() => {
     console.log(serverData);
-    console.log(correlation);
   }, [serverData, correlation])
 
   const handleSelectMethod = e => {
-    axios.post(`http://127.0.0.1:8000/api/${e.target.value}-trend/`, file)
-    .then(response => response.data)
-    .then(data => {setServerData(data); setCorrelation(data.correlation)})
+    e.preventDefault();
+    const fileData = new FormData()
+    fileData.append('file', file)
+    axios.post(`http://127.0.0.1:8000/api/${e.target.value}-trend/`, fileData)
+    .then(response => {
+      if (response.data.error) return setError(response.data.error);
+      return response.data
+    })
+    .then(data => setServerData(data))
+    .catch(error => console.log(error.message))
   }
   
   return (
     <div className="App">
-      <input type="file" onChange={e => setFile(e.target.files[0])}/>
-      {file && (
-        <>
-          <button onClick={e => handleSelectMethod(e) } value='linear'>Linear</button>
-          <button onClick={e => handleSelectMethod(e) } value='logarithmic'>Logarithmic</button>
-          <button onClick={e => handleSelectMethod(e) } value='hyperbolic'>Hyperbolic</button>
-          <button onClick={e => handleSelectMethod(e) } value='smoothing'>Smoothing</button>
-          <p>{correlation}</p>
-        </>
-      )}
+      <form encType="multipart/form-data">
+        <input type="file" onChange={e => setFile(e.target.files[0])}/>
+        {file && (
+          <>
+            <button onClick={e => handleSelectMethod(e) } value='linear'>Linear</button>
+            <button onClick={e => handleSelectMethod(e) } value='logarithmic'>Logarithmic</button>
+            <button onClick={e => handleSelectMethod(e) } value='hyperbolic'>Hyperbolic</button>
+            <button onClick={e => handleSelectMethod(e) } value='smoothing'>Smoothing</button>
+            <p>{serverData?.correlation}</p>
+            {error && !serverData?.data && <p>{error}</p>}
+          </>
+        )}
+      </form>
     </div>
   );
 }
