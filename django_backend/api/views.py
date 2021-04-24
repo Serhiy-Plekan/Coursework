7a6e6e0f25174881033ae9c07bfdc89624c0bcdd
Serhiy-Plekan <serhiyplx@gmail.com>
2021-04-24 23:49:20 +0300

    xls file support

--- django_backend/api/views.py
+++ django_backend/api/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+import pandas as pd
+
 from coursework import linear_time_trend, logarithmic_time_trend, hyperbolic_time_trend, smoothing
 
 from django.http import HttpResponse
@@ -8,34 +11,48 @@ from django.http import HttpResponse
 
 @api_view(['GET'])
 def index(request):
-    data="penis"
+    data="hello"
     return Response(data)
 
 
 @api_view(['POST'])
 def linearTrend(request):
-    data = [float(i.replace(',', '.')) for i in [j for j in request.data][0].split(' ')]
-    final = linear_time_trend(data)
-    print(final)
-    return Response(final)
+    # data = [float(i.replace(',', '.')) for i in [j for j in request.data][0].split(' ')]
+    if '.xls' in str(request.FILES['file']):
+        user_data = pd.read_excel(request.FILES['file'])
+        data = linear_time_trend(user_data)
+        return Response(data)
+    error_message = 'Please, choose the correct file format'
+    return Response({'error': error_message})
+    
+
 
 @api_view(['POST'])
 def logarithmicTrend(request):
-    data = [float(i.replace(',', '.')) for i in [j for j in request.data][0].split(' ')]
-    final = logarithmic_time_trend(data)
-    print(final)
-    return Response(final)
+    if '.xls' in str(request.FILES['file']):
+        user_data = pd.read_excel(request.FILES['file'])
+        data = logarithmic_time_trend(user_data)
+        return Response(data)
+    error_message = 'Please, choose the correct file format'
+    return Response({'error': error_message})
+    
 
 @api_view(['POST'])
 def hyperbolicTrend(request):
-    data = [float(i.replace(',', '.')) for i in [j for j in request.data][0].split(' ')]
-    final = hyperbolic_time_trend(data)
-    print(final)
-    return Response(final)
+    if '.xls' in str(request.FILES['file']):
+        user_data = pd.read_excel(request.FILES['file'])
+        data = hyperbolic_time_trend(user_data)
+        return Response(data)
+    error_message = 'Please, choose the correct file format'
+    return Response({'error': error_message})
 
+    
 @api_view(['POST'])
 def dataSmoothing(request):
-    data = [float(i.replace(',', '.')) for i in [j for j in request.data][0].split(' ')]
-    final = smoothing(data)
-    print(final)
-    return Response(final)
+    if '.xls' in str(request.FILES['file']):
+        user_data = pd.read_excel(request.FILES['file'])
+        data = smoothing(user_data)
+        return Response(data)
+    error_message = 'Please, choose the correct file format'
+    return Response({'error': error_message})
+    
