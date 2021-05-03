37e70393b3deb1b7f997bb92ca0700987f19c2ee
OlehTereshchuk <olehtereshchuk@ukr.net>
2021-05-03 19:19:16 +0300

    slide animation

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,46 +1,19 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
+import { HashRouter } from "react-router-dom";
+import { useRoutes } from "./router";
+import SideMenu from "./components/SideMenu";
 
-function App() {
-  const [file, setFile] = useState(null);
-  const [serverData, setServerData] = useState({});
-  const [error, setError] = useState('')
-  const [correlation, setCorrelation] = useState(0);
+const App = () => {
+  const routes = useRoutes();
 
-  useEffect(() => {
-    console.log(serverData);
-  }, [serverData, correlation])
-
-  const handleSelectMethod = e => {
-    e.preventDefault();
-    const fileData = new FormData()
-    fileData.append('file', file)
-    axios.post(`http://127.0.0.1:8000/api/${e.target.value}-trend/`, fileData)
-    .then(response => {
-      if (response.data.error) return setError(response.data.error);
-      return response.data
-    })
-    .then(data => setServerData(data))
-    .catch(error => console.log(error.message))
-  }
-  
   return (
-    <div className="App">
-      <form encType="multipart/form-data">
-        <input type="file" onChange={e => setFile(e.target.files[0])}/>
-        {file && (
-          <>
-            <button onClick={e => handleSelectMethod(e) } value='linear'>Linear</button>
-            <button onClick={e => handleSelectMethod(e) } value='logarithmic'>Logarithmic</button>
-            <button onClick={e => handleSelectMethod(e) } value='hyperbolic'>Hyperbolic</button>
-            <button onClick={e => handleSelectMethod(e) } value='smoothing'>Smoothing</button>
-            <p>{serverData?.correlation}</p>
-            {error && !serverData?.data && <p>{error}</p>}
-          </>
-        )}
-      </form>
-    </div>
+    <HashRouter>
+      <div className="container">
+        <SideMenu />
+        {routes}
+      </div>
+    </HashRouter>
   );
-}
+};
 
 export default App;
